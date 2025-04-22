a91217b8ed2c53a75cdfc4158eae62a3c260ce32
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2025-04-22 17:32:42 -0300

    Update theme colors

--- lib/screens/home_screen/home.dart
+++ lib/screens/home_screen/home.dart
@@ -1,5 +1,8 @@
 import 'package:alubank/components/box_card.dart';
+import 'package:alubank/components/color_dot.dart';
+import 'package:alubank/components/content_division.dart';
 import 'package:alubank/components/sections/header.dart';
+import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -9,7 +12,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[Header(), BoxCard(boxContent: Text('Hello'))],
+        children: <Widget>[Header(), BoxCard(boxContent: ContentDivision())],
       ),
     );
   }
