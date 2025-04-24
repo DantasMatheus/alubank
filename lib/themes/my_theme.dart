c6aa7969ad3598aabb3aec554e1bb9db03ce8218
Matheus Dantas <95482834+DantasMatheus@users.noreply.github.com>
2025-04-23 21:19:38 -0300

    Update complete recent_activity widget

--- lib/themes/my_theme.dart
+++ lib/themes/my_theme.dart
@@ -4,9 +4,18 @@ import 'package:flutter/material.dart';
 ThemeData myTheme = ThemeData(
   primarySwatch: ThemeColors.primaryColor,
   primaryColor: ThemeColors.primaryColor,
+  progressIndicatorTheme: ProgressIndicatorThemeData(
+    color: ThemeColors.primaryColor,
+  ),
   brightness: Brightness.dark,
   textTheme: TextTheme(
     bodyMedium: TextStyle(fontSize: 16.0),
     bodyLarge: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      foregroundColor: ThemeColors.primaryColor,
+      textStyle: TextStyle(fontSize: 16.0),
+    ),
   ),
 );
